2a6db996b5d83c1da73803fb55d0697ea993b78e
ozgen <ozgn45@gmail.com>
2024-07-15 01:25:23 +0200

    fix indentation for add-user-table

--- cmd/migrate/migrations/20240714225942_add-user-table.up.sql
+++ cmd/migrate/migrations/20240714225942_add-user-table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
-                                     id SERIAL PRIMARY KEY,
-                                     firstName VARCHAR(255) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    firstName VARCHAR(255) NOT NULL,
     lastName VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,

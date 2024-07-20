0efc8a2bfe99f42c43038526afd747ea3073abbd
ozgen <ozgn45@gmail.com>
2024-07-21 00:17:57 +0200

    Add custom errors and test

---
+++ customErrors/customError.go
@@ -0,0 +1,27 @@
+package customErrors
+
+import "fmt"
+
+type NotFoundError struct {
+	ID string
+}
+
+func (e *NotFoundError) Error() string {
+	return fmt.Sprintf("item with ID %s not found", e.ID)
+}
+
+type NotInitError struct {
+	ID string
+}
+
+func (e *NotInitError) Error() string {
+	return fmt.Sprintf("item with ID %s not initialized", e.ID)
+}
+
+type AlreadyInitError struct {
+	ID string
+}
+
+func (e *AlreadyInitError) Error() string {
+	return fmt.Sprintf("item with ID %s is already initialized", e.ID)
+}

bb7a3bbd9f3e8fd38e97a060a62bbe85f12eb581
ozgen <ozgn45@gmail.com>
2024-07-20 23:28:07 +0200

    Add initialize camera endpoint

--- service/camerametadata/store_test.go
+++ service/camerametadata/store_test.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 	"go-sample-rest-api/types"
 	"testing"
 	"time"
@@ -22,7 +23,7 @@ func setupMockDB(t *testing.T) (*sql.DB, sqlmock.Sqlmock, func()) {
 	return db, mock, cleanup
 }
 
-func TestStore_CreateCameraMetaDataCreateCameraMetaData(t *testing.T) {
+func TestStore_CreateCameraMetadata(t *testing.T) {
 	t.Run("CreateCameraMetaData_withValidData_toCreateCameraMetadata", func(t *testing.T) {
 		// arrange
 		db, mock, cleanup := setupMockDB(t)
@@ -30,10 +31,15 @@ func TestStore_CreateCameraMetaDataCreateCameraMetaData(t *testing.T) {
 
 		store := Store{db}
 
+		timeNow := time.Now()
+		nullTime := sql.NullTime{
+			Time:  timeNow,
+			Valid: true,
+		}
 		camera := types.CameraMetadata{
 			CameraName:      "Test Camera",
 			FirmwareVersion: "v1.0",
-			CreatedAt:       time.Now(),
+			CreatedAt:       nullTime,
 		}
 
 		expectedID := uuid.New().String()
@@ -44,7 +50,7 @@ func TestStore_CreateCameraMetaDataCreateCameraMetaData(t *testing.T) {
 				AddRow(expectedID, camera.CameraName, camera.FirmwareVersion, camera.CreatedAt))
 
 		// act
-		savedCamera, err := store.CreateCameraMetaData(camera)
+		savedCamera, err := store.CreateCameraMetadata(camera)
 
 		// assert
 		if err := mock.ExpectationsWereMet(); err != nil {
@@ -65,10 +71,15 @@ func TestStore_CreateCameraMetaDataCreateCameraMetaData(t *testing.T) {
 
 		store := Store{db}
 
+		timeNow := time.Now()
+		nullTime := sql.NullTime{
+			Time:  timeNow,
+			Valid: true,
+		}
 		camera := types.CameraMetadata{
 			CameraName:      "Test Camera",
 			FirmwareVersion: "v1.0",
-			CreatedAt:       time.Now(),
+			CreatedAt:       nullTime,
 		}
 
 		mock.ExpectQuery(`INSERT INTO camera_metadata`).
@@ -76,7 +87,196 @@ func TestStore_CreateCameraMetaDataCreateCameraMetaData(t *testing.T) {
 			WillReturnError(sql.ErrConnDone)
 
 		// act
-		_, err := store.CreateCameraMetaData(camera)
+		_, err := store.CreateCameraMetadata(camera)
+
+		// assert
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("there were unfulfilled expectations: %s", err)
+		}
+
+		if err == nil {
+			t.Errorf("expected an error, got none")
+		}
+
+		if err != sql.ErrConnDone {
+			t.Errorf("expected a sql.ErrConnDone error, got %v", err)
+		}
+	})
+}
+
+func TestStore_GetCameraMetadataByID(t *testing.T) {
+	t.Run("GetCameraMetadataByID_withValidData_toGetCameraMetadata", func(t *testing.T) {
+		// arrange
+		db, mock, cleanup := setupMockDB(t)
+		defer cleanup()
+
+		store := Store{db}
+
+		camID := uuid.New().String()
+
+		rows := sqlmock.NewRows([]string{"cam_id", "image_id", "camera_name", "firmware_version", "container_name", "name_of_stored_picture", "created_at", "onboarded_at", "initialized_at"}).
+			AddRow(camID, nil, "Test Camera", "v1.0", nil, nil, time.Now(), time.Now(), time.Now())
+		mock.ExpectQuery(`^SELECT cam_id, image_id, camera_name, firmware_version, container_name, name_of_stored_picture, created_at, onboarded_at, initialized_at FROM camera_metadata WHERE cam_id = \$1$`).
+			WithArgs(camID).
+			WillReturnRows(rows)
+
+		// act
+		cameraMetadata, err := store.GetCameraMetadataByID(camID)
+
+		// assert
+		assert.NoError(t, mock.ExpectationsWereMet())
+
+		assert.NoError(t, err)
+		assert.NotNil(t, cameraMetadata)
+		assert.Equal(t, camID, cameraMetadata.CamID)
+
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("there were unfulfilled expectations: %s", err)
+		}
+
+		if err != nil {
+			t.Errorf("expected no error, but got %s", err)
+		}
+	})
+	t.Run("GetCameraMetadataByID_withNoRows_toReturnError", func(t *testing.T) {
+		// arrange
+		db, mock, cleanup := setupMockDB(t)
+		defer cleanup()
+		store := Store{db}
+
+		camID := "non-existent-id"
+		mock.ExpectQuery(`^SELECT.*FROM camera_metadata WHERE cam_id = \$1$`).
+			WithArgs(camID).
+			WillReturnError(sql.ErrNoRows)
+
+		// act
+		cameraMetadata, err := store.GetCameraMetadataByID(camID)
+
+		// assert
+		assert.Error(t, err)
+		assert.Nil(t, cameraMetadata)
+		assert.Equal(t, "no camera metadata found with ID: non-existent-id", err.Error())
+
+	})
+	t.Run("CreateCameraMetaData_withError_toReturnError", func(t *testing.T) {
+		// arrange
+		db, mock, cleanup := setupMockDB(t)
+		defer cleanup()
+
+		store := Store{db}
+
+		camID := "non-existent-id"
+		mock.ExpectQuery(`^SELECT.*FROM camera_metadata WHERE cam_id = \$1$`).
+			WithArgs(camID).
+			WillReturnError(sql.ErrConnDone)
+
+		// act
+		_, err := store.GetCameraMetadataByID(camID)
+
+		// assert
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("there were unfulfilled expectations: %s", err)
+		}
+
+		if err == nil {
+			t.Errorf("expected an error, got none")
+		}
+
+		if err != sql.ErrConnDone {
+			t.Errorf("expected a sql.ErrConnDone error, got %v", err)
+		}
+	})
+}
+func TestStore_UpdateCameraMetadata(t *testing.T) {
+	t.Run("UpdateCameraMetadata_withValidData_toGetCameraMetadata", func(t *testing.T) {
+		// arrange
+		db, mock, cleanup := setupMockDB(t)
+		defer cleanup()
+
+		store := Store{db}
+
+		cam := types.CameraMetadata{
+			CamID:               "123",
+			CameraName:          "Test Camera",
+			FirmwareVersion:     "v1.0",
+			ContainerName:       sql.NullString{String: "Test Container", Valid: true},
+			NameOfStoredPicture: sql.NullString{String: "test_picture.jpg", Valid: true},
+			CreatedAt:           sql.NullTime{Time: time.Now(), Valid: true},
+			OnboardedAt:         sql.NullTime{Time: time.Now(), Valid: true},
+			InitializedAt:       sql.NullTime{Time: time.Now(), Valid: true},
+		}
+
+		mock.ExpectExec("UPDATE camera_metadata").WithArgs(
+			cam.CameraName, cam.FirmwareVersion, cam.ContainerName, cam.NameOfStoredPicture,
+			cam.CreatedAt, cam.OnboardedAt, cam.InitializedAt, cam.CamID,
+		).WillReturnResult(sqlmock.NewResult(1, 1))
+
+		// act
+		updatedCamera, err := store.UpdateCameraMetadata(cam)
+
+		// assert
+		assert.NoError(t, mock.ExpectationsWereMet(), "Expectations were not met")
+
+		assert.NoError(t, err, "Error was not expected when updating camera metadata")
+		assert.NotNil(t, updatedCamera, "Updated camera metadata should not be nil")
+		assert.Equal(t, cam.CamID, updatedCamera.CamID, "Updated camera metadata should have the same CamID")
+	})
+	t.Run("UpdateCameraMetadata_withNoRows_toReturnError", func(t *testing.T) {
+		// arrange
+		db, mock, cleanup := setupMockDB(t)
+		defer cleanup()
+		store := Store{db}
+
+		cam := types.CameraMetadata{
+			CamID:               "123",
+			CameraName:          "Test Camera",
+			FirmwareVersion:     "v1.0",
+			ContainerName:       sql.NullString{String: "Test Container", Valid: true},
+			NameOfStoredPicture: sql.NullString{String: "test_picture.jpg", Valid: true},
+			CreatedAt:           sql.NullTime{Time: time.Now(), Valid: true},
+			OnboardedAt:         sql.NullTime{Time: time.Now(), Valid: true},
+			InitializedAt:       sql.NullTime{Time: time.Now(), Valid: true},
+		}
+
+		mock.ExpectExec("UPDATE camera_metadata").WithArgs(
+			cam.CameraName, cam.FirmwareVersion, cam.ContainerName, cam.NameOfStoredPicture,
+			cam.CreatedAt, cam.OnboardedAt, cam.InitializedAt, cam.CamID,
+		).WillReturnError(sql.ErrNoRows)
+
+		// act
+		updatedCamera, err := store.UpdateCameraMetadata(cam)
+
+		// assert
+		assert.NoError(t, mock.ExpectationsWereMet(), "Expectations were not met")
+
+		assert.Error(t, err, "Expected an error when no rows are affected")
+		assert.Nil(t, updatedCamera, "No camera metadata should be returned when no rows are affected")
+	})
+	t.Run("UpdateCameraMetadata_withError_toReturnError", func(t *testing.T) {
+		// arrange
+		db, mock, cleanup := setupMockDB(t)
+		defer cleanup()
+
+		store := Store{db}
+
+		cam := types.CameraMetadata{
+			CamID:               "123",
+			CameraName:          "Test Camera",
+			FirmwareVersion:     "v1.0",
+			ContainerName:       sql.NullString{String: "Test Container", Valid: true},
+			NameOfStoredPicture: sql.NullString{String: "test_picture.jpg", Valid: true},
+			CreatedAt:           sql.NullTime{Time: time.Now(), Valid: true},
+			OnboardedAt:         sql.NullTime{Time: time.Now(), Valid: true},
+			InitializedAt:       sql.NullTime{Time: time.Now(), Valid: true},
+		}
+
+		mock.ExpectExec("UPDATE camera_metadata").WithArgs(
+			cam.CameraName, cam.FirmwareVersion, cam.ContainerName, cam.NameOfStoredPicture,
+			cam.CreatedAt, cam.OnboardedAt, cam.InitializedAt, cam.CamID,
+		).WillReturnError(sql.ErrConnDone)
+
+		// act
+		_, err := store.UpdateCameraMetadata(cam)
 
 		// assert
 		if err := mock.ExpectationsWereMet(); err != nil {

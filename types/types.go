7a00bd9862c412e7cb5e14123392fbd66223d7c8
ozgen <ozgn45@gmail.com>
2024-07-25 23:38:28 +0200

    Add swagger documentation to the rest endpoints

--- types/types.go
+++ types/types.go
@@ -23,6 +23,13 @@ type LoginUserPayload struct {
 	Password string `json:"password" validate:"required"`
 }
 
+// HTTPError example used for error responses.
+// swagger:model HTTPError
+type HTTPError struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
 type UserStore interface {
 	GetUserByEmail(email string) (*User, error)
 	GetUserByID(id int) (*User, error)
